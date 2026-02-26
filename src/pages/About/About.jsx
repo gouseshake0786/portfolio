ef12e588b637ad75cd12689115882f10d5ac3a9b
Mahamad Gouse <mahammadgouse432@gmail.com>
2026-02-26 06:26:39 +0530

    Update About page with new developer name

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,4 @@
-import HeroImg from "@/assets/images/hero.jpg";
+import HeroImg from "@/assets/images/hero.jpg1";
 import OlovaLogo from "@/assets/images/olova.png";
 
 export default function About() {
@@ -24,7 +24,7 @@ export default function About() {
 
             <div className="relative space-y-4">
               <p className="text-white">
-                Hello! I'm Nazmul Hossain, a passionate JavaScript developer
+                Hello! I'm Mahamad Gouse, a passionate JavaScript developer
                 specializing in creating innovative web solutions and
                 user-friendly interfaces.{" "}
                 <span className="font-bold text-white">
@@ -51,7 +51,7 @@ export default function About() {
 
                   <div className="mt-6 space-y-3">
                     <cite className="block font-medium text-white">
-                      Nazmul Hossain, Creator of
+                      Mahamad Gouse, Creator of
                     </cite>
                     <div className="flex items-center gap-2">
                       <img
