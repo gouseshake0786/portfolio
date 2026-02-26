f17a9e62494098104639faca920ebd34a04cf493
Mahamad Gouse <mahammadgouse432@gmail.com>
2026-02-26 20:33:53 +0530

    added projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -5,45 +5,46 @@ import PropTypes from "prop-types";
 
 const projects = [
   {
-    title: "Olova! A Lightweight JavaScript Library",
+    title: "Hospital Management System (HMS)",
     description:
-      "A lightweight JavaScript library for creating beautiful, responsive UI components.",
+      "A serverless Hospital Management System built with Vanilla JavaScript and Google Apps Script, using Google Sheets as a database for secure, role-based operations. It streamlines healthcare workflows with OTP authentication, automated PDF generation for prescriptions, and GST-compliant pharmacy billing.",
     src: "rock.jpg",
-    link: "https://i.postimg.cc/DwgWTfP0/Annotation-2025-03-19-113338.png",
+    link: "../../assets/1.png",
     color: "#5196fd",
-    githubLink: "https://github.com/olovajs/olova",
-    liveLink: "https://olova.js.org/",
-  },
-  {
-    title: "A sleek portfolio built with React and Tailwind CSS ",
-    description:
-      "A sleek portfolio built with React and Tailwind CSS to showcase your skills, projects, and experience in a modern design.",
-    src: "tree.jpg",
-    link: "https://i.postimg.cc/J75CKyrs/Annotation-2025-04-01-203959.png",
-    color: "#8f89ff",
-    githubLink: "https://github.com/seraprogrammer/portfolio",
-    liveLink: "https://codervai.vercel.app",
-  },
-  {
-    title: "🚀 CodeWhisperer",
-    description:
-      "🚀 CodeWhisperer A powerful online code editor built with React and Tailwind CSS. Featuring real-time code execution, syntax highlighting, multi-language support, and a sleek UI. Start coding instantly! 💻✨",
-    src: "water.jpg",
-    link: "https://i.postimg.cc/J4jPVFY0/Annotation-2025-04-01-204723.png",
-    color: "#fff",
-    githubLink: "https://github.com/seraprogrammer/codewhisperer",
-    liveLink: "https://codewhisperer.vercel.app/",
-  },
-  {
-    title: "CodeKori 🔥",
-    description:
-      "CodeKori is a powerful online code editor built with React and Tailwind CSS. Featuring real-time code execution, syntax highlighting, multi-language support, and a sleek UI. Start coding instantly! 💻✨",
-    src: "house.jpg",
-    link: "https://i.postimg.cc/cHQr4fpR/Annotation-2025-04-01-205350.png",
-    color: "#ed649e",
-    githubLink: "https://github.com/seraprogrammer/CodeKori",
-    liveLink: "https://codekori.js.org",
-  },
+    githubLink: "https://github.com/gouseshake0786/HMSDental",
+    liveLink: "https://gouseshake0786.github.io/HMSDental/",
+  }
+  // },
+  // {
+  //   title: "A sleek portfolio built with React and Tailwind CSS ",
+  //   description:
+  //     "A sleek portfolio built with React and Tailwind CSS to showcase your skills, projects, and experience in a modern design.",
+  //   src: "tree.jpg",
+  //   link: "https://i.postimg.cc/J75CKyrs/Annotation-2025-04-01-203959.png",
+  //   color: "#8f89ff",
+  //   githubLink: "https://github.com/seraprogrammer/portfolio",
+  //   liveLink: "https://codervai.vercel.app",
+  // },
+  // {
+  //   title: "🚀 CodeWhisperer",
+  //   description:
+  //     "🚀 CodeWhisperer A powerful online code editor built with React and Tailwind CSS. Featuring real-time code execution, syntax highlighting, multi-language support, and a sleek UI. Start coding instantly! 💻✨",
+  //   src: "water.jpg",
+  //   link: "https://i.postimg.cc/J4jPVFY0/Annotation-2025-04-01-204723.png",
+  //   color: "#fff",
+  //   githubLink: "https://github.com/seraprogrammer/codewhisperer",
+  //   liveLink: "https://codewhisperer.vercel.app/",
+  // },
+  // {
+  //   title: "CodeKori 🔥",
+  //   description:
+  //     "CodeKori is a powerful online code editor built with React and Tailwind CSS. Featuring real-time code execution, syntax highlighting, multi-language support, and a sleek UI. Start coding instantly! 💻✨",
+  //   src: "house.jpg",
+  //   link: "https://i.postimg.cc/cHQr4fpR/Annotation-2025-04-01-205350.png",
+  //   color: "#ed649e",
+  //   githubLink: "https://github.com/seraprogrammer/CodeKori",
+  //   liveLink: "https://codekori.js.org",
+  // },
 ];
 
 export default function Projects() {
